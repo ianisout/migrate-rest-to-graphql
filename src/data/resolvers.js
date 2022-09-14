f2701b0b891cfc925d29885b19f26094a2170480
Ian LS <iansouza.sjc@gmail.com>
2022-09-14 14:48:51 -0300

    delete contact

--- src/data/resolvers.js
+++ src/data/resolvers.js
@@ -41,6 +41,14 @@ export const resolvers = {
           else resolve(contact);
         });
       });
+    },
+    deleteContact: (root, {id}) => {
+      return new Promise((resolve, object) => {
+        Contacts.remove({_id: id}, (err) => {
+          if (err) reject(err);
+          else resolve('Successfully deleted contact.');
+        });
+      })
     }
   },
 };
