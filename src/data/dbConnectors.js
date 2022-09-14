89fd60c8ce372c4c50e4c0410ea80ff05460b5e0
Ian LS <iansouza.sjc@gmail.com>
2022-09-14 13:32:25 -0300

    refactor project setup

---
+++ src/data/dbConnectors.js
@@ -0,0 +1,16 @@
+export const ContactSchema = new Schema({
+  firstName: {
+      type: String,
+      required: 'Enter a first name'
+  },
+  lastName: {
+      type: String,
+      required: 'Enter a last name'
+  },
+  email: {
+      type: String
+  },
+  company: {
+      type: String
+  },
+});
